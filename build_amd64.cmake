a93d06babc03f81ed34b11e636877ac45a9fa34c
Alexander Sidorov <a.sidorov@rogii.com>
2021-06-03 11:56:06 +0400

    change name from "cmake" to "rogii" to avoid name collision.

--- build_amd64.cmake
+++ build_amd64.cmake
@@ -1,4 +1,4 @@
 execute_process(
     COMMAND
-        "${CMAKE_COMMAND}" -E time "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/cmake/build_amd64.cmake"
+        "${CMAKE_COMMAND}" -E time "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/rogii/build_amd64.cmake"
 )
--- build_x86.cmake
+++ build_x86.cmake
@@ -1,4 +1,4 @@
 execute_process(
     COMMAND
-        "${CMAKE_COMMAND}" -E time "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/cmake/build_x86.cmake"
+        "${CMAKE_COMMAND}" -E time "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/rogii/build_x86.cmake"
 )
